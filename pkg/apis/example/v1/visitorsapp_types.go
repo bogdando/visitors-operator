708a18a462c86c9a18597c3d0af85483cd80ef72
Bogdan Dobrelya <bogdando@mail.ru>
2020-04-17 15:05:33 +0200

    add properties
    Signed-off-by: Bogdan Dobrelya <bogdando@mail.ru>

--- pkg/apis/example/v1/visitorsapp_types.go
+++ pkg/apis/example/v1/visitorsapp_types.go
@@ -8,22 +8,29 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // VisitorsAppSpec defines the desired state of VisitorsApp
+// +k8s:openapi-gen=true
 type VisitorsAppSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Size  int32  `json:"size,omitempty"`
+	Title string `json:"title,omitempty"`
 }
 
 // VisitorsAppStatus defines the observed state of VisitorsApp
+// +k8s:openapi-gen=true
 type VisitorsAppStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	BackendImage  string `json:"backendImage,omitempty"`
+	FrontendImage string `json:"frontendImage,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // VisitorsApp is the Schema for the visitorsapps API
+// +k8s:openapi-gen=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=visitorsapps,scope=Namespaced
 type VisitorsApp struct {
